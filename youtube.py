53fa2127737a4e99a6d241990ae1e54626925333
Elise <justin555@live.nl>
2022-08-08 09:31:48 +0000

    finished the Youtube check task

--- youtube.py
+++ youtube.py
@@ -3,9 +3,10 @@ import requests
 import re
 import urllib
 import urllib.request
+import EmbedBuilder
 from discord.ext import tasks
 
-@tasks.loop(seconds=7200)
+@tasks.loop(seconds=60)
 async def checkforVideos(client):
   with open("youtubeJson.json", "r") as ytData:
     data=json.load(ytData)
@@ -20,7 +21,8 @@ async def checkforVideos(client):
       #anwsers used from stackoverflow 
       #https://stackoverflow.com/questions/59627108/retrieve-youtube-video-title-using-api-python
       #https://github.com/AdvicSaha443/Discord.py-Youtube-Notification-Bot/blob/main/main.py
-      #https://www.youtube.com/watch?v=qZ6kcQJxbzQ          #https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html#discord.ext.tasks.Loop.start
+      #https://www.youtube.com/watch?v=qZ6kcQJxbzQ          #https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html
+      #discord.ext.tasks.Loop.start
       
       try:
         latest_video_url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
@@ -33,19 +35,29 @@ async def checkforVideos(client):
         data[str(youtube_channel)]["latest_video_url"] = latest_video_url
 
         #Dumping the new youtube lateste url into the json file
-        with open("youtube.json", "w") as ytJson:
+        with open("youtubeJson.json", "w") as ytJson:
           json.dump(data, ytJson)
 
         discord_channel_id = data[str(youtube_channel)]['notifying_discord_channel']
         discord_channel = client.get_channel(int(discord_channel_id))
 
-        params = {"format": "json", "url": latest_video_url}
+        vidId = re.search('(?<="videoId":").*?(?=")', html).group()
+        parmas = {"format": "json", "url": latest_video_url}
         url = "https://www.youtube.com/oembed"
         query_string = urllib.parse.urlencode(parmas)
         url = url + "?" + query_string
         
         with urllib.request.urlopen(url) as response:
-          response_text = response_read()
+          response_text = response.read()
           data = json.loads(response_text.decode())
-          await discord_channel.send("Dont worry Elise is testing something no panic ")
-          await discord_channel.send(data['title'])
+          if("Perfect" in data['title'] or "Divaroadmap" in data["title"]):
+            vidTitle = "<@&934500064364216390> <@203095887264743424> has uploaded a Project diva video go check it out 💜"
+            embedTitle = "Project diva video"
+            embedDescription = "🎀 Elise has uploaded a new project diva video. Care to see if she got a perfect or got a new divaroadmap. See what happends by checking the title of the embed 🎀"
+          else:
+            vidTitle = "<@&934500064364216390> <@203095887264743424> has uploaded a lets play video. Go check it out 💜"
+            embedTitle = "Lets play video"
+            embedDescription = "🎀 Elise has uploaded a new lets play video. Is it horror or a fun lets play video. Well its hard to tell but i see a new upload so. Care to check it out? Click on the title to take you to the vid. 🎀"
+
+          await discord_channel.send(vidTitle)
+          await EmbedBuilder.BuildEmbed(None, embedTitle, embedDescription, "http://img.youtube.com/vi/" + vidId + "/0.jpg", None, "🎀 New Vid Upload 🎀", discord_channel, latest_video_url)
